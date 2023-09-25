8958ac994ceb20956ae77e4719eeffd353c1a25c
Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2023-09-25 20:33:45 +0200

    Add files via upload

---
+++ PROYECTO/JAVASCRIPT/Scripts.js
@@ -0,0 +1,26 @@
+function enviar1() {
+    var q = document.getElementById("nom").value;
+    var w = document.getElementById("nac").value;
+    var e = document.getElementById("telef").value;
+    var r = document.getElementById("naci").value;
+    var t = document.getElementById("gen").value;
+    var y = document.getElementById("correo").value;
+    var u = document.getElementById("dir").value;
+    document.location = "../HTML/Index2.html";
+}
+
+function enviar2() {
+    var i = document.getElementById("descri").value;
+    document.location = "../HTML/Index3.html";
+}
+
+function enviar3() {
+    var o = document.getElementById("pues").value;
+    var p = document.getElementById("empleR").value;
+    var a = document.getElementById("direC").value;
+    var s = document.getElementById("acti").value;
+    var d = document.getElementById("desde").value;
+    var f = document.getElementById("mailE").value;
+    var g = document.getElementById("hasta").value;
+    var h = document.getElementById("webE").value;
+}
